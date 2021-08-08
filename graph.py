74fc1206755156efd022cd262c15ec0a50cfa02c
chiragbaid7 <chiragbaid7@gmail.com>
2021-08-08 17:47:34 +0530

    graph

---
+++ graph.py
@@ -0,0 +1,96 @@
+from collections import defaultdict, deque
+class Graph:
+    def __init__(self,nodes):
+        #this will create empty list as a value for a non existent key
+        self.list=defaultdict(list)
+        self.nodes=nodes
+    def AddEdge(self,x,y):
+        self.list[x].append(y)
+        self.list[y].append(x)
+
+    def printGraph(self):
+        for key in self.list:
+            neigh=self.list[key] 
+            print(key,neigh)
+
+    def BFS(self,src):
+        queue=deque()
+        #a node can be visited twice so to avoid maintain a visited array
+        visited=[False]*self.nodes
+        visited[src]=True 
+        queue.append(src)
+        while(queue):
+            key =queue.popleft()
+            print(key)
+            for neigh in self.list[key]:
+                if(not visited[neigh]):
+                    visited[neigh]=True
+                    queue.append(neigh)
+
+    def __util_DFS(self,src,visited):
+        visited[src]=True 
+        print(src)
+        for neigh in self.list[src]:
+            if(not visited[neigh]):
+                self.__util_DFS(neigh,visited)
+        return
+        
+    def DFS(self,src):
+        visited=[False]*self.nodes 
+        self.__util_DFS(src,visited)
+
+    def undirected_graph_cycle_BFS(self,src):
+        queue=deque()
+        visited=[False]*self.nodes 
+        parent=[-1]*self.nodes 
+        parent[src]=src 
+        visited[src]=True
+        queue.append(src)
+
+        while(queue):
+            node=queue.popleft()
+            for neigh in self.list[node]:
+                if(visited and parent[node]==neigh):
+                    continue
+                if(visited[neigh] and node!=parent[neigh]):
+                    return True 
+                else:
+                    visited[neigh]=True 
+                    parent[neigh]=node 
+                    queue.append(neigh)
+        return False 
+        
+    def __cycle_dfs(self,visited,src,parent):
+        if(visited[src] and parent!=src):
+            return True
+        visited[src]=True 
+        for child in self.list[src]:
+            if(child==parent): #if the node is the parent of src then continue and traverse from the other end 
+                continue 
+            elif(self.__cycle_dfs(visited,child,src)):
+                return True 
+        return False 
+
+
+    def undirected_graph_cycle_DFS(self,src):
+        visited=[False]*self.nodes 
+        """
+            dfs traversal you traverse along the current path till reach the dead end
+            and backtrack and traverse from that node.Reapeat this untill all the nodes are 
+            visited.
+        """
+        return self.__cycle_dfs(visited,src,src)
+
+
+def main():
+    g=Graph(5)
+    g.AddEdge(1,2)
+    g.AddEdge(2,3)
+    g.AddEdge(2,4)
+    g.AddEdge(3,4)
+    #g.BFS(2)
+    #g.DFS(1)
+    print(g.undirected_graph_cycle_BFS(1))
+    print(g.undirected_graph_cycle_DFS(1))
+if __name__=="__main__":
+    main()
