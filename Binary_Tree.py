13be1205149bfbe9a53eda155b59c3cf8d7e77f3
Chirag Baid <37933427+chiragbaid7@users.noreply.github.com>
2024-05-11 15:12:50 +0530

    Update Binary_Tree.py

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -57,3 +57,54 @@ root.insert(14)
 root.print_tree()
 
 print(root.printHeight())
+
+
+
+# 2.0
+from collections import deque
+class Node:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+class Tree:
+    def __init__(self):
+        self.root = None
+    # BFS - level order traversal
+    '''
+        If a pointer(L or R) is null we insert a node and return
+        till then we recursively travel Left and the Right
+    '''
+    def height(self, node):
+        if(self.node == None):
+            return 0
+        return max(self.height(self.node.left), self.height(self.node.right)) + 1
+
+    def printLevelNodes(nLevel, root, level):
+        if(root == None):
+            return
+        if(nLevel == level):
+            print(root.data)
+            return
+        printLevelNodes(nLevel,root.left, level + 1)
+        printLevelNodes(nLevel,root.right, level + 1)
+    def recursiveLevelOrder(self,node):
+        height = self.height(node)
+        for i in range (1, height + 1):
+            printLevelNodes(i, root, 1)
+    def iterativeBFS(self, data):
+        if(self.root == None):
+            self.root = Node(data)
+        else:
+            q= dequeue()
+            q.append(self.root)
+            while(len(q)):
+                root = q.popleft()
+                if(root.left):
+                    root.left = Node(data)
+                    break
+                elif(root.right):
+                    root.right = Node(data)
+                    break
+                q.append(root.left)
+                q.append(root.right)
