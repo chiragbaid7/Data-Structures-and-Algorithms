9c10d754af9d6f8569c8002086e771e11ad687e5
Chirag Baid <37933427+chiragbaid7@users.noreply.github.com>
2024-06-13 22:54:56 +0530

    Update topology_sort.py

--- topology_sort.py
+++ topology_sort.py
@@ -4,17 +4,17 @@ class Graph:
 
     def __init__(self,nodes):
         #this will create empty list as a value for a non existent key
-        self.list=defaultdict(list)
+        self.graph=defaultdict(list)
         self.nodes=nodes
 
     def AddEdge(self,x,y):
-        self.list[x].append(y)
+        self.graph[x].append(y)
         #self.list[y].append(x)
 
     def topology_sort(self):        
         indegree=[0]*self.nodes 
-        for parent in self.list:
-            for node in self.list[parent]:
+        for parent in self.graph:
+            for node in self.graph[parent]:
                 indegree[node]+=1
         queue=deque()
         for node in self.list:
@@ -23,7 +23,7 @@ class Graph:
         while(queue):
             node=queue.popleft()
             print(node)
-            for neigh in self.list[node]:
+            for neigh in self.graph[node]:
                 indegree[neigh]-=1
                 if(indegree[neigh]==0):
                     queue.append(neigh)
@@ -38,13 +38,48 @@ class Graph:
         return 
 
     def topology_sort_dfs(self):
-        stack=deque()
+        stack = deque()
         visited=[False]*self.nodes 
         #for disconnected graphs 
-        for node in self.list:
+        for node in self.graph:
             if(not visited[node]):
                 self.__util_dfs(node,stack,visited)
         print(stack)
+
+    def utilsNewDFS(self, stack, node, visited):
+        visited[node] = True
+        for neighbour in graph[node]:
+            if(not visited[node):
+                self.utilsNewDFS(stack, node, visited)
+        #Leaf node
+        stack.append(node)
+    def topologyDFS(self):
+        stack = []
+        visited = [False]*self.nodes
+        for node in self.graph:
+            #Explore from univisted nodes only
+            if(not visited[node]):
+                self.utilsNewDFS(stack, node, visited):
+    def topologyBFS(self):
+        indegree = [0] * self.nodes
+        for node in self.graph:
+            for neighbour in self.grpah[node]:
+                indegree[neighbour]+=1
+        queue = deque()
+        for node in self.graph:
+            if(indegree[node] == 0):
+                queue.append(node)
+        while(queue):
+            node = queue.popleft()
+            print(node)
+            for neighbour in self.graph[node]:
+                indegree[neighbour]-=1
+                if(indegree[neighbour] == 0):
+                    queue.append(neighbour)
+        for node in indegree:
+            if(node > 0):
+                #Graph contains cycle
+                return False
         
 def main():
     g=Graph(6)
